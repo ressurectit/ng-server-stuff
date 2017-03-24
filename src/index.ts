3791ae39c57316a10e7fd821898b0cdb03465649
kukjevov <kukjevov@gmail.com>
2017-03-24 15:30:52 +0100

    BUGFIX - fixed forgotten export of serverRenderFactory

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {ServerTransferStateRestModule} from './modules/serverTransferStateRest.module';
 export {ServerTransferStateService} from './transferState/serverTransferState.service';
 export {ServerInterceptableHttpModule} from './modules/serverInterceptableHttp.module';
+export {serverRenderFactory} from './misc/serverRenderFactory';
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = function(options)
         },
         externals:
         [
+            /^fs$/,
             /^@angular\/.*$/,
             /^@ng\/.*$/,
             /^rxjs\/.*$/
